d2725d9f5c8fa82f2043b41c317d9b445f0e0c74
Riccardo Casula <riccardocasula@aridhia.net>
2025-02-06 11:45:10 +0000

    gh-pages: Deleted all but docs as it's the only thing needed

--- controller/__main__.py
+++
@@ -1,7 +0,0 @@
-from .controller import start
-
-print("Starting the controller")
-
-if __name__ == "__main__":
-    start()
-
--- scripts/tests/pylint-entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-pylint controller --output /tmp/pylint.xml

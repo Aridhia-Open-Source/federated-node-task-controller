940464032dbaf5effa7311666589ae4694fbf670
r-casula <riccardo.casula@aridhia.com>
2025-04-03 16:52:27 +0100

    azure-bugfixes: Try catch to autorestart the watcher

--- controller/__main__.py
+++ controller/__main__.py
@@ -1,10 +1,14 @@
 """
 Entrypoint for the FNTC
 """
-
+from urllib3.exceptions import ProtocolError
 from .controller import start
 
 print("Starting the controller")
 
 if __name__ == "__main__":
-    start()
+    try:
+        start()
+    except ProtocolError:
+        print("Connection expired. Reconnecting...")
+        start()

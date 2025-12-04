28861d460130270ea789e2aa717c6389ed63c833
Riccardo Casula <riccardo.casula@aridhia.com>
2025-12-04 14:57:14 +0000

    1.6.0 (#45)
    * Version bump to 1.4.0
    
    * 206-unique-resources: unique resource prefixed (#39)
    
    * 206-unique-resources: unique resource prefixed
    
    * 206-unique-resources: Restored the Roles as ClusterRoles as we can't manage cluster resources with namespaced roles and bindings
    
    * Helm tests (#36)
    
    * helm-tests: First tests
    
    * helm-tests: Added azure test cases, polished the extra empty spaces in the templates
    
    * helm-tests: Added failure cases
    
    * helm-tests: Added helm tests to the pipeline
    
    * helm-tests: Fixed path mispelled
    
    * helm-tests: Changed the mocked secrets/cm to reflect the real structure
    
    * helm-tests: Fixed an issue with the new review value in global. Added a "areWeSubchart" template to help with this
    
    * 233-rfc-name-violation (#42)
    
    * 233-rfc-name-violation: Renamed all link user jobs to be link-x-user
    
    * 233-rfc-name-violation: Updated CHANGELOG
    
    * 233-rfc-name-violation: Replace the job suffix with a datetime timestamp
    
    * 233-rfc-name-violation: Changed the name to be link-user and the suffix would be a timestamp to avoid conflicts. Labels for that job were updated as well
    
    * Version bump to 1.5.0
    
    * Version bump to 1.6.0
    
    * HEAD: develop: Reverted crd group name
    
    * 173 keycloak migration (#41)
    
    * 173-keycloak-migration: Renamed old keycloak secret key
    
    * 173-keycloak-migration: version bump
    
    * 173-keycloak-migration: Started transitioning towards async
    
    * 173-keycloak-migration: Fully moved to async tasks.
    
    * 173-keycloak-migration: db_query is not passed at all if not provided
    
    * 173-keycloak-migration: Fixed the async task for pod monitor to not be waited for
    
    * 173-keycloak-migration: Added health check for idp initializer to minimize job retry cooldown
    
    * 173-keycloak-migration: Improved with connection error exception handling
    
    * 173-keycloak-migration: Added a retry for login as kc takes time to init
    
    * 173-keycloak-migration: Added params instead of inline query parameters for get user
    
    * 173-keycloak-migration: Fixed test that still referenced to the query string
    
    ---------
    
    Co-authored-by: PHEMS bot <phemsbot@phems.com>

--- controller/helpers/keycloak_helper.py
+++ controller/helpers/keycloak_helper.py
@@ -9,9 +9,10 @@ Both kubernetes and keycloak standard operations
 import os
 import logging
 
+import httpx
+
 from exceptions import KeycloakException
 from helpers.kubernetes_helper import KubernetesV1
-from helpers.request_helper import client as requests
 from const import KC_USER, KC_HOST
 
 logger = logging.getLogger('keycloak_helper')
@@ -23,66 +24,68 @@ REALM = "FederatedNode"
 KC_HOST = os.getenv("KC_HOST")
 
 
-def get_keycloak_secret() -> str:
+async def get_keycloak_secret() -> str:
     """
     Simple generalization to get keycloak secret, as it has
     fixed keys in it
     """
-    return KubernetesV1().get_secret('kc-secrets', 'KEYCLOAK_GLOBAL_CLIENT_SECRET')
+    return KubernetesV1().get_secret('kc-secrets', 'KEYCLOAK_SECRET')
 
-def get_keycloak_admin_pass() -> str:
+async def get_keycloak_admin_pass() -> str:
     """
     Simple generalization to get the specific admin key
     for the keycloak secret
     """
     return KubernetesV1().get_secret('kc-secrets', 'KEYCLOAK_ADMIN_PASSWORD')
 
-def get_admin_token() -> str:
+async def get_admin_token() -> str:
     """
     Simply send a request to Keycloak to get the admin token
     based on the password fetched from the k8s secret itself
     """
-    admin_resp = requests.post(
+    admin_resp = httpx.post(
         f"{KC_HOST}/realms/{REALM}/protocol/openid-connect/token",
         data={
             'client_id': KEYCLOAK_CLIENT,
-            'client_secret': get_keycloak_secret(),
+            'client_secret': await get_keycloak_secret(),
             'grant_type': 'password',
             'username': KC_USER,
-            'password': get_keycloak_admin_pass()
+            'password': await get_keycloak_admin_pass()
         },
         headers = {
             'Content-Type': 'application/x-www-form-urlencoded'
         }
     )
-    if not admin_resp.ok:
+    if admin_resp.status_code > 299:
         raise KeycloakException("Failed to login")
     return admin_resp.json()["access_token"]
 
 
-def get_user(email:str=None, username:str=None, idpId:str=None) -> dict:
+async def get_user(email:str=None, username:str=None, idpId:str=None) -> dict:
     """
     Method to return a dictionary representing a Keycloak user
     """
-    admin_token = get_admin_token()
+    admin_token = await get_admin_token()
     if idpId:
-        user_response = requests.get(
-            f"{KC_HOST}/admin/realms/{REALM}/users?idpUserId={idpId}",
-            headers={"Authorization": f"Bearer {admin_token}"}
-        )
+        params = {"idpUserId": idpId}
     elif email:
-        user_response = requests.get(
-            f"{KC_HOST}/admin/realms/{REALM}/users?email={email}&exact=true",
-            headers={"Authorization": f"Bearer {admin_token}"}
-        )
+        params={
+            "email": email,
+            "exact": True
+        }
     elif username:
-        user_response = requests.get(
-            f"{KC_HOST}/admin/realms/{REALM}/users?username={username}&exact=true",
-            headers={"Authorization": f"Bearer {admin_token}"}
-        )
+        params={
+            "email": email,
+            "exact": True
+        }
     else:
         raise KeycloakException("Either email or username are needed")
-    if not user_response.ok:
+
+    user_response = httpx.get(f"{KC_HOST}/admin/realms/{REALM}/users",
+        params=params,
+        headers={"Authorization": f"Bearer {admin_token}"}
+    )
+    if user_response.status_code > 299:
         raise KeycloakException(user_response.content.decode())
     if len(user_response.json()):
         return user_response.json()[0]
@@ -90,19 +93,19 @@ def get_user(email:str=None, username:str=None, idpId:str=None) -> dict:
     raise KeycloakException(f"User {email or idpId or username} not found")
 
 
-def impersonate_user(user_id:str) -> str:
+async def impersonate_user(user_id:str) -> str:
     """
     Given a user id, it will return a refresh_token for it
     through the admin-level user
     """
-    exchange_resp = requests.post(
+    exchange_resp = httpx.post(
         f"{KC_HOST}/realms/{REALM}/protocol/openid-connect/token",
         data={
-            'client_secret': get_keycloak_secret(), # Target client
+            'client_secret': await get_keycloak_secret(), # Target client
             'client_id': KEYCLOAK_CLIENT, #Target client
             'grant_type': 'urn:ietf:params:oauth:grant-type:token-exchange',
             'requested_token_type': 'urn:ietf:params:oauth:token-type:refresh_token',
-            'subject_token': get_admin_token(),
+            'subject_token': await get_admin_token(),
             'requested_subject': user_id,
             'audience': KEYCLOAK_CLIENT
         },
@@ -110,6 +113,6 @@ def impersonate_user(user_id:str) -> str:
             'Content-Type': 'application/x-www-form-urlencoded'
         }
     )
-    if not exchange_resp.ok:
+    if exchange_resp.status_code > 299:
         raise KeycloakException(exchange_resp.content.decode())
     return exchange_resp.json()["refresh_token"]
